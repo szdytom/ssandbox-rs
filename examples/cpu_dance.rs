b6dab966afb0768cc11381acd778bf9b938ad15f
szdytom <szdytom@163.com>
2021-03-01 13:43:11 +0800

    [example] add cpu_dance

--- examples/cpu_dance.rs
+++ examples/cpu_dance.rs
@@ -1,5 +1,7 @@
 // You need to have a alpine image @ /root/sandbox/image to run this example
 // You will get a shell inside the container.
+// You will also need a command named "loop" @ /bin/loop inside the root image.
+// The source of loop can be found @ https://gist.githubusercontent.com/szdytom/ec2d99a41477787a8c85f6ec73af4ca2/raw/54fb722cb3ed60ef902573ee2ce503d8760ec723/loop.c
 
 #![feature(box_syntax)]
 
@@ -12,9 +14,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     config.fs.push(box filesystem::MountReadOnlyBindFs::from("/root/sandbox/image".to_string()));
     config.fs.push(box filesystem::MountExtraFs::new());
     config.cgroup_limits.set_fork_limit(3);
+    config.target_executable = "/bin/loop".to_string();
     let mut c = Container::from(config);
     c.start()?;
-    c.wait()?;
+    for _i in 0..15 {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        c.freeze()?;
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        c.thaw()?;
+    }
+    c.terminate()?;
     println!("Finished!");
     Ok(())
 }
